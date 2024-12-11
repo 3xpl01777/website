887e7c02c8f9bf85aa5ec0488dd88c38a53b63ad
3xpl01777 <0x55555555.exe@gmail.com>
2024-12-11 07:08:25 -0600

    base

---
+++ js/commands.js
@@ -0,0 +1,122 @@
+const github = "https://github.com/3xpl01777/";
+const linkedin = "";
+const instagram = "";
+
+const email = "mailto:yann@ynncstslv.dev";
+
+const password = "39e17d32a3f738be5877b13880096d87";
+
+banner = [
+  "<br />",
+  "                                               dP            dP          ",
+  "                                               88            88          ",
+  " dP    dP 88d888b. 88d888b. .d8888b. .d8888b. d8888P .d8888b. 88 dP   .dP ",
+  " 88    88 88'  `88 88'  `88 88'  `'' Y8ooooo.   88   Y8ooooo. 88 88   d8' ",
+  " 88.  .88 88    88 88    88 88.  ...       88   88         88 88 88 .88'  ",
+  " `8888P88 dP    dP dP    dP `88888P' `88888P'   dP   `88888P' dP 8888P'   ",
+  "     .88                                                                 ",
+  "  d8888P                                                                  ",
+  "<br />",
+  '<span class="index">YNNCSTSLV @ Not A Corporation. All rights reserved.</span>',
+  "<br />",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+  "<br />",
+];
+
+help = [
+  "<br />",
+  '<span class="command">whoami</span>         About me',
+  '<span class="command">experience</span>     Work experience',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">contact</span>        Send me a message',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">history</span>        See command history',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">help</span>           You know this one',
+  "<br />",
+];
+
+whoami = [];
+
+experience = [];
+
+projects = [];
+
+social = [];
+
+contact = [];
+
+secret = [
+  "<br />",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br />",
+];
+
+// whoami = [
+//   "<br />",
+//   "Hi, I'm 3XPL017!ðŸ‘‹",
+//   "<br />",
+//   "I am a software engineer specialized in cybersecurity, operating under this alias,",
+//   "dedicating my skills and time to conduct penetration tests, identify critical",
+//   "vulnerabilities and share my findings with the community to help improve overall",
+//   "security.",
+//   "<br />",
+//   "I am also a passionate advocate for open-source tools and resources, believing that",
+//   "collaboration and transparency are key to advancing the field of computer science.",
+//   "<br />",
+//   "This page serves as a showcase of my work, research and contributions to the community,",
+//   "<br />",
+// ];
+
+// whoami = [
+//   "<br>",
+//   "The paradox of â€œWho am I?â€ is: we never know, but, we constantly find out.",
+//   "<br>"
+// ];
+
+// social = [
+//   "<br />",
+//   'github         <a href="' +
+//     github +
+//     '" target="_blank">github/3xpl01777' +
+//     "</a>",
+//   'hackerone      <a href="' +
+//     hackerone +
+//     '" target="_blank">hackerone/3xpl01777' +
+//     "</a>",
+//   'bugcrowd       <a href="' +
+//     bugcrowd +
+//     '" target="_blank">bugcrowd/3xpl01777' +
+//     "</a>",
+//   "<br />",
+// ];
+
+// secret = [
+//   "<br />",
+//   '<span class="command">sudo</span>           Only use if you\'re admin',
+//   "<br />",
+// ];
+
+// projects = [
+//   "<br>",
+//   "Still curating... most projects are offline, on GitHub, or confidential.",
+//   "<br>"
+// ];
+
+// help = [
+//   "<br>",
+//   '<span class="command">whois</span>          Who is Forrest?',
+//   '<span class="command">whoami</span>         Who are you?',
+//   '<span class="command">video</span>          View YouTube videos',
+//   '<span class="command">social</span>         Display social networks',
+//   '<span class="command">secret</span>         Find the password',
+//   '<span class="command">projects</span>       View coding projects',
+//   '<span class="command">history</span>        View command history',
+//   '<span class="command">help</span>           You obviously already know what this does',
+//   '<span class="command">email</span>          Do not email me',
+//   '<span class="command">clear</span>          Clear terminal',
+//   '<span class="command">banner</span>         Display the header',
+//   "<br>",
+// ];
